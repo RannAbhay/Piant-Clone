d9272e92853e070c42360839f950906e5e197ed3
kunalbhardwaj911 <neerusharma2684@gmail.com>
2025-04-27 13:15:19 +0530

    Added canvas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
 
 function App() {
+  const canvasRef = useRef(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Paint App</h1>
+      <canvas
+        ref={canvasRef}
+        width={800}
+        height={600}
+        style={{ border: '1px solid black' }}
+      />
     </div>
   );
 }
